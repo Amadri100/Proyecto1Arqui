3578758a05f3975f25471231ddd4e2e5c0442399
Amadri <andresmadrigalcampos@gmail.com>
2026-06-16 20:23:50 -0600

    Listo

--- Proyecto1Arqui/Proyecto1Arqui/Programa.asm
+++ Proyecto1Arqui/Proyecto1Arqui/Programa.asm
@@ -12,7 +12,8 @@ ExitProcess proto
     labelP db "Ingrese p: ", 0
     valorA  db "%lld", 0       ;Toma un entero de 64bits
     valorP  db "%lld", 0
-    labelResultado db "a = %lld, p = %lld", 10, 0
+    labelNoHayInverso db "El número no tiene inverso multiplicativo modular", 10, 0
+    labelHayInverso db "El inverso de a = %lld del modulo p = %lld es: %lld", 10, 0
     a dq ?
     p dq ?
 
@@ -24,19 +25,74 @@ main PROC
 
     call leerEntrada ; llama a la función para leer los valores
 
-    mov RAX, [RSP]      ; a
-    mov RCX, [RSP+8]    ; p
+    mov RAX, [RSP] ; RAX <- a
+    mov RBX, [RSP+8] ; RBX <- b
+    mov [a], RAX    ;Se asignan los valores a los espacios de memoria
+    mov [p], RBX    ;
 
-    lea RCX, labelResultado
-    mov RDX, [RSP]      ; a
-    mov R8,  [RSP+8]    ; p
-    call printf
+    sub RSP, 16   ;Reservar espacio para las salidas
+    push QWORD PTR [p]
+    push QWORD PTR [a] 
+    call algoritmoExtendidoEuclides
 
-    add RSP, 10h        ; limpiar slots
+    push QWORD PTR [RSP + 24]
+    push QWORD PTR [RSP + 24]
+    call output
 
-    mov ECX, 0          ; cédigo de éxito
+    ;add RSP, 48        ; limpiar slots
+
+    mov ECX, 0          ; codigo de salida
     call ExitProcess
+
+   
+
 main ENDP
+
+output PROC
+
+;RSP = Return ADRESS
+;RSP + 8 = MCD
+;RSP + 16 = x del MCD (Falta normalizarlo)
+
+;IF MCD != 1 -> no hay inverso multiplicativo modular
+mov RAX, [RSP + 8] ; RAX <- MCD
+cmp RAX, 1
+jne no_hay
+si_hay:
+    ;normaliza x
+    mov RDX, 0          ; limpia RDX
+    mov RAX, [RSP + 16] ; RAX <- x
+    CQO                 ; Extiende el signo de RAX a RDX::RAX
+    IDIV [p]            ;RDX <- x%p
+
+    mov R15, RDX        ; R15 <- x%p 
+    cmp R15, 0
+
+    Jl es_negativo ;si R15 < 0 -> hay que sumarle el divisor
+    ;si x%p < 0 -> x%p + p == inverso modular
+    ;de lo contrario x%p == inverso modular
+    jmp impresion
+
+    es_negativo:
+        add R15, [p] ; R15 <- R15 + p
+
+    impresion:
+    sub RSP , 28h  ;Shadow space
+    ;LLamar impresion
+    lea RCX, labelHayInverso
+    mov RDX, [a]      ; a
+    mov R8,  [p]    ; p
+    mov R9,  R15    ; inverso
+    call printf
+    add RSP, 28h
+    ret
+no_hay:
+    sub RSP , 28h  ;Shadow space
+    lea RCX, labelNoHayInverso
+    call printf
+    add RSP, 28h
+    ret
+output ENDP
 
 leerEntrada PROC
     push RBX            ; preservar RBX, RSP-8
@@ -104,19 +160,21 @@ while_b_le_0:
     mov RBX, RAX ; temporalmente mueve q  a RBX
     mov R15, RDX ; mueve r a R15
 
-    mul R9  ; RAX <- q*x1
+    imul R9  ; RAX <- q*x1
     
     mov R13, R10 ; x <- x2
     sub R13, RAX ; x <- x2 - q*x1
 
     mov RAX, RBX ; RAX <- q
-    mul R11      ; RAX <- q*y1
+    imul R11      ; RAX <- q*y1
 
     mov R14, R12 ; y <- y2
     sub R14, RAX ; y <- y2 - q*y1
 
     mov RBX, [RSP + 16]; RBX <- B
     mov [RSP + 8], RBX ; A <- B (Mediante RBX)
+
+    mov [RSP + 16], R15 ; B <- r
 
     mov R10, R9 ; x2 <- x1
     mov R9, R13 ; x1 <- x
